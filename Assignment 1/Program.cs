85de0413d62d9338568d1b52f2c202f31018eac8
Anas Al-Adaileh <70060855+Anas-Adaileh@users.noreply.github.com>
2026-02-12 19:01:46 +0300

    Add project files.

---
+++ Assignment 1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please enter the invoice amount");
+
+double amount = Convert.ToDouble(Console.ReadLine());
+
+if (amount >= 500)
+{
+    amount = amount * (1 - 0.2);
+    Console.WriteLine($"Final Price: {amount}");
+}
+else if (amount >= 300 && amount <= 499)
+{
+    amount = amount * (1 - 0.1);
+    Console.WriteLine($"Final Price: {amount}");
+}
+else if (amount < 300)
+{
+    Console.WriteLine($"Final Price: {amount}");
+}
