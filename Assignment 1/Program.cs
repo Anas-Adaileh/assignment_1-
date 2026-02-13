ed753f80e620f65a67f05a6f8380ae11d7624379
Anas Al-Adaileh <70060855+Anas-Adaileh@users.noreply.github.com>
2026-02-13 12:39:50 +0300

    The Eng Solve

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -5,7 +5,11 @@ Console.WriteLine("Please enter the invoice amount");
 
 double amount = Convert.ToDouble(Console.ReadLine());
 
-if (amount >= 500)
+if (amount < 0)
+{
+    Console.WriteLine($"Invalied Amount {amount}");
+} 
+else if (amount >= 500)
 {
     amount = amount * (1 - 0.2);
     Console.WriteLine($"Final Price: {amount}");
@@ -19,3 +23,27 @@ else if (amount < 300)
 {
     Console.WriteLine($"Final Price: {amount}");
 }
+
+// another way to solve it
+
+Console.WriteLine("Please enter the invoice amount");
+double _amount = Convert.ToDouble( Console.ReadLine() );
+
+double discountPercentage = 0.0;
+double discountAmount = 0.00;
+double finalAmount = 0.00;
+
+if (_amount < 0)
+{
+    Console.WriteLine($"Invalied Amount {_amount}");
+} else if (_amount <= 50)
+{
+    discountPercentage =  0.2;
+} else if (_amount < 500 && _amount >= 300)
+{
+    discountPercentage =  0.1;
+}
+
+discountAmount = _amount * discountPercentage;
+finalAmount = _amount - discountAmount;
+Console.WriteLine($"The final amount after discount {discountPercentage} is: {finalAmount}");
